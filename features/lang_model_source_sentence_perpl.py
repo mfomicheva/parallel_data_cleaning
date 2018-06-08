ec9d39f840ee1e1da40f09d7b6f3fc84fed1a450
marina <mari.fomicheva@gmail.com>
2018-06-08 18:40:20 +0200

    Added names

--- features/lang_model_source_sentence_perpl.py
+++ features/lang_model_source_sentence_perpl.py
@@ -3,6 +3,8 @@ from features.lang_model_source_sentence_proba import LangModelSourceSentencePro
 
 class LangModelSourceSentencePerpl(LangModelSourceSentenceProba):
 
+    name = 'lang_model_source_sentence_perp'
+
     def run(self):
         self.score = self._lang_model_score(self.src)
 
--- features/lang_model_target_sentence_perpl.py
+++ features/lang_model_target_sentence_perpl.py
@@ -3,5 +3,7 @@ from features.lang_model_target_sentence_proba import LangModelTargetSentencePro
 
 class LangModelTargetSentencePerpl(LangModelTargetSentenceProba):
 
+    name = 'lang_model_target_sentence_perp'
+
     def _lang_model_score(self, line):
         return self.resources.tgt_lm.perplexity(line)
