d671814d75a768574ded04a037c62f95c9ca919c
marina <mari.fomicheva@gmail.com>
2018-06-08 16:53:46 +0200

    Moved arg parsing

--- extract_features.py
+++ extract_features.py
@@ -1,25 +1,28 @@
-import getopt
+import argparse
 import sys
 import re
 import importlib
 
 from features.feature_names import feature_names
-from parse_args import args
 
 
-def _read_args(args):
-    opts, args = getopt.getopt(args, 'hs:t:ls:lt:', ['input_file='])
-    path_src = None
-    path_tgt = None
-    for opt, arg in opts:
-        if opt in ('-s', '--source_file'):
-            path_src = arg
-        elif opt in ('-t', '--target_file'):
-            path_tgt = arg
-        else:
-            sys.stdout.write('-s <source_file> -t <target_file>')
-            sys.exit()
-    return path_src, path_tgt
+def _parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-s', '--src_path', help='source path'
+    )
+    parser.add_argument(
+        '-t', '--tgt_path', help='target path'
+    )
+    parser.add_argument(
+        '-slm', '--source_lang_model', help='source language model',
+        default='toy_resources/lm.tok.en'
+    )
+    parser.add_argument(
+        '-tlm', '--target_lang_model', help='target language model',
+        default='toy_resources/lm.tok.de'
+    )
+    return parser.parse_args()
 
 
 def _load_feature_class(feature_name):
@@ -37,6 +40,7 @@ def _snake_to_camel(name):
 
 
 def main():
+    args = _parse_arguments()
     with open(args.src_path) as f_src:
         with open(args.tgt_path) as f_tgt:
             for src, tgt in zip(f_src.readlines(), f_tgt.readlines()):
