1788668abb110f59c5e367b54bfa34d3b5b7fb69
marina <mari.fomicheva@gmail.com>
2018-06-12 16:32:07 +0200

    Improved names

--- patterns.py
+++ patterns.py
@@ -3,33 +3,33 @@
 class Patterns:
 
     @staticmethod
-    def count(input):
-        return len(input)
+    def count(units):
+        return len(units)
 
     @staticmethod
-    def ratio(input):
-        return len(input[0]) / len(input[1]) if len(input[1]) else 0
+    def ratio(units_pair):
+        return len(units_pair[0]) / len(units_pair[1]) if len(units_pair[1]) else 0
 
     @staticmethod
-    def inverse_ratio(input):
-        return len(input[1]) / len(input[0]) if len(input[0]) else 0
+    def inverse_ratio(units_pair):
+        return len(units_pair[1]) / len(units_pair[0]) if len(units_pair[0]) else 0
 
-    @staticmethod
-    def difference(input):
-        return abs(len(input[0]) - len(input[1]))
-
-    def difference_normalized(self, input):
-        return self._zero_safe(self.difference(input), self._maximum_length(input))
-
-    def jaccard_index(self, input):
-        set1 = set(input[0])
-        set2 = set(input[1])
+    def jaccard_index(self, units_pair):
+        set1 = set(units_pair[0])
+        set2 = set(units_pair[1])
         return self._zero_safe(len(set1.intersection(set2)), len(set1.union(set2)))
+
+    def difference_normalized(self, units_pair):
+        return self._zero_safe(self.difference(units_pair), self._maximum_length(units_pair))
+
+    @staticmethod
+    def difference(units_pair):
+        return abs(len(units_pair[0]) - len(units_pair[1]))
 
     @staticmethod
     def _zero_safe(a, b):
         return a / b if b else 0.
 
     @staticmethod
-    def _maximum_length(input):
-        return max(len(input[0]), len(input[1]))
+    def _maximum_length(units_pair):
+        return max(len(units_pair[0]), len(units_pair[1]))
