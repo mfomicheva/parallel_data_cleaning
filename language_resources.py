5352e95a6751311ac5512aad1b88dee8e09807e4
Jesús González-Rubio <jesus.g@webinterpret.com>
2018-06-13 13:28:17 +0200

    bla

--- language_resources.py
+++ language_resources.py
@@ -1,3 +1,4 @@
+import sys
 import kenlm
 
 
@@ -8,13 +9,20 @@ class Resources:
         self.loaded = dict()
 
     def load_resources(self):
+        sys.stderr.write('LOADING LM: %s ... ' % self.paths['language_model'])
+        sys.stderr.flush()
         self.loaded['language_model'] = kenlm.Model(self.paths['language_model'])
+        sys.stderr.write('DONE\n')
+
+        sys.stderr.write('LOADING LEX: %s ... ' % self.paths['lexical_table'])
+        sys.stderr.flush()
         self.loaded['lexical_table'] = self._load_table(self.paths['lexical_table'])
+        sys.stderr.write('DONE\n')
 
     @staticmethod
     def _load_table(path):
         table = {}
         for line in open(path):
             parts = line.strip().split()
-            table[parts[0], parts[1]] = parts[2]
+            table[parts[0], parts[1]] = float(parts[2])
         return table
