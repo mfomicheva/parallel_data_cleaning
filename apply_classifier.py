7298ceef0ae3e8f02eb19906c43f666b1f8ffe69
marina <mari.fomicheva@gmail.com>
2018-06-22 12:20:16 +0200

    Added script to apply classifier

--- apply_classifier.py
+++ apply_classifier.py
@@ -5,11 +5,10 @@ def main():
     features_path = '/home/jesus/news-commentary-v13.de-en.tok.low.features.negative.out_1K'
     model_path = '/home/jesus/models/news-commentary.p'
     classifier = Classifier()
-    features = classifier.load_features(features_path)
     classifier.load_model(model_path)
-    predictions = classifier._classifier.predict(features)
-    for pred in predictions:
-        print(pred)
+    for row in classifier.lazy_load(features_path):
+        pred = classifier._classifier.predict_proba([row])
+        print(pred[0][1])
 
 
 if __name__ == '__main__':
