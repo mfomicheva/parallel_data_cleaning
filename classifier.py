a31f1c7d2964d781cfb0b1d1ecdb99d267e5396a
marina <mari.fomicheva@gmail.com>
2018-06-22 10:28:50 +0200

    Added scripts for training classifier

---
+++ classifier.py
@@ -0,0 +1,27 @@
+import pickle
+import numpy as np
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics.classification import f1_score
+
+
+class Classifier:
+
+    def __init__(self):
+        self._classifier = GradientBoostingClassifier()
+
+    def train_model(self, positive_X, negative_X):
+        features = np.concatenate((positive_X, negative_X), axis=0)
+        labels = np.concatenate((
+            np.ones((len(positive_X), 1)),
+            np.zeros((len(negative_X), 1)),
+        ))
+        x_train, x_test, y_train, y_test = train_test_split(
+            features, labels, train_size=0.7)
+        self._classifier.fit(x_train, y_train)
+        preds = self._classifier.predict(x_test)
+        print('F1: {}'.format(f1_score(y_test, preds)))
+
+    def save_model(self, path):
+        pickle.dump(self._classifier, open(path, 'wb'))
