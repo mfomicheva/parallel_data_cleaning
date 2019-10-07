dde36e72806faf75e9eed069a692515b21774656
mfomicheva <mfomicheva@at-languagesolutions.com>
2019-10-07 11:44:46 +0200

    Added required=True for arguments

--- parallel_data_cleaning/bin/generate_synthetic_data.py
+++ parallel_data_cleaning/bin/generate_synthetic_data.py
@@ -28,10 +28,10 @@ def parse_arguments():
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--src_path', help='source path', default='toy_corpus/src.en')
-    parser.add_argument('-t', '--tgt_path', help='target path', default='toy_corpus/tgt.de')
-    parser.add_argument('-os', '--out_path_src', help='output path source', default='output/src.neg.en')
-    parser.add_argument('-ot', '--out_path_tgt', help='output path target', default='output/tgt.neg.en')
+    parser.add_argument('-s', '--src_path', help='source path', default='toy_corpus/src.en', required=True)
+    parser.add_argument('-t', '--tgt_path', help='target path', default='toy_corpus/tgt.de', required=True)
+    parser.add_argument('-os', '--out_path_src', help='output path source', default='output/src.neg.en', required=True)
+    parser.add_argument('-ot', '--out_path_tgt', help='output path target', default='output/tgt.neg.en', required=True)
     args = parse_args_with_help(parser)
     generate_synthetic_data(args.src_path, args.tgt_path, args.out_path_src, args.out_path_tgt)
 
